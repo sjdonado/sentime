087a962069b1c7eb809121998d48adde30efc661
Juan Rodríguez <jsrd98@gmail.com>
2020-03-28 23:49:52 -0500

    feat: Get tweets by cities
     - Dynamic table scraper results by cities

--- app/routes/search.py
+++ app/routes/search.py
@@ -5,6 +5,9 @@ import twint
 import asyncio
 import threading
 
+from datetime import date
+from datetime import timedelta
+
 from flask import session, Blueprint
 
 from .. import socketio, app
@@ -21,28 +24,46 @@ search_bp = Blueprint('search_bp', __name__,
 
 def launch_query(c):
   asyncio.set_event_loop(asyncio.new_event_loop())
-  twint.run.Search(c)
 
-  tweets = [t.__dict__ for t in twint.output.tweets_list]
-  # print("DATA => {}".format(tweets), flush=True)
-  socketio.emit('tweets', { 'message': 'Finished', 'data': tweets })
+  tweets_by_cities = []
+  last_len = 0
+
+  for idx, city in enumerate(cities):
+    # TODO: Fit radio based on city boundaries
+    c.Geo = "{},{},5km".format(
+      city['geometry']['location']['lat'],
+      city['geometry']['location']['lng']
+    )
+    twint.run.Search(c)
+    tweets = [t.__dict__ for t in twint.output.tweets_list[last_len:]]
+    last_len = len(tweets)
+    socketio.emit('tweets', {
+      'status': 'processing',
+      'progress': (idx + 1) / 32,
+      'data': {
+        'city': city,
+        'results': last_len,
+        'tweets': map(lambda t: t['tweet'], tweets)
+      }
+    })
+    tweets_by_cities.append({
+      'city': city,
+      'tweets': map(lambda t: t['tweet'], tweets)
+    })
+
+  socketio.emit('tweets', { 'status': 'finished', 'data': tweets_by_cities })
 
 @socketio.on('search')
 def search(message):
-  print(message)
   data = json.loads(message)
   session['text'] = data['text']
   c = twint.Config()
   c.Search = data['text']
+  c.Since = date.today() - timedelta(days=7)
   c.Store_object = True
   c.Location = True
-  c.Limit = 100
+  c.Hide_output = True
+  c.Show_hashtags = False
 
-  # TODO: Add each city thread to a Queue
-
-  c.Geo = "{},{},5km".format(cities[0]['geometry']['location']['lat'], cities[0]['geometry']['location']['lng'])
-
-  # for city in cities:
-  #   c.Geo = "{},{},5km".format(city['geometry']['location']['lat'], city['geometry']['location']['lng'])
-  socketio.emit('tweets', { 'message': 'Processing...', 'data': [] })
+  socketio.emit('tweets', { 'status': 'started', 'data': [] })
   threading.Thread(target=launch_query, args=(c,), daemon=True).start()

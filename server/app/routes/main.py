f881b3068bf57508561f031756140df58cd622c7
Juan Rodriguez <jsrd98@gmail.com>
2020-05-25 02:43:10 -0500

    refactor: Server prod dockerfile

--- server/app/routes/main.py
+++ server/app/routes/main.py
@@ -1,15 +1,9 @@
-from flask import render_template, session, Blueprint
+from flask import Blueprint, jsonify
 
 from .. import app
 
-main_bp = Blueprint('main_bp', __name__,
-                    template_folder='templates',
-                    static_folder='static')
+main_bp = Blueprint('main_bp', __name__)
 
 @main_bp.route('/')
 def index():
-  return render_template(
-    'index.html',
-    title = "Sentime",
-    text = session.get('text') if session.get('text') else '',
-  )
+  return jsonify({ 'status': 'ok' })
--- server/wsgi.py
+++ server/wsgi.py
@@ -1,7 +1,10 @@
 #!/bin/env python
+import os
 from app import create_app, socketio
+
+port = int(os.environ.get("PORT", 5000))
 
 app = create_app()
 
 if __name__ == '__main__':
-  socketio.run(app, host='0.0.0.0')
+  socketio.run(app, host='0.0.0.0', port=port)
